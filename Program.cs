578a1dd75d53cab09145719ce2c01f87a4b5eaad
Joaquim Barreto <joaquim@barreto.co>
2019-09-26 19:14:21 +0200

    Done

--- Program.cs
+++ Program.cs
@@ -11,6 +11,26 @@ namespace BuildTree
     {
         static void Main(string[] args)
         {
+            Tree<char> charTree = null;
+            InsertIntoTree<char>(ref charTree, 'D', 'i', 'c', 'k', 'h', 'e', 'a', 'd');
+            string sortedData = charTree.WalkTree();
+            Console.WriteLine($"Sorted Data is: {sortedData}");
+        }
+
+        static void InsertIntoTree<TItem>(ref Tree<TItem> tree, params TItem[] data) where TItem : IComparable<TItem>
+        {
+            foreach (TItem datum in data)
+            {
+                if (tree == null)
+                {
+                    tree = new Tree<TItem>(datum);
+                }
+                else
+                {
+                    tree.Insert(datum);
+                }
+            }
+            
         }
     }
 }
